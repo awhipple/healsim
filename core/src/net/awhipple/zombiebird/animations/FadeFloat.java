36d0e15ea84a9c8721142cae70a83266d7fbca30
awhipple <a.rgwhip@gmail.com>
2017-01-03 05:31:16 -0800

    A bit more float animation clean up

--- core/src/net/awhipple/zombiebird/animations/FadeFloat.java
+++ core/src/net/awhipple/zombiebird/animations/FadeFloat.java
@@ -2,7 +2,7 @@ package net.awhipple.zombiebird.animations;
 
 public class FadeFloat extends Animation {
 
-  float floatSpeed, fadeInSpeed, fadeOutSpeed, fadeInTime, floatTime, fadeOutTime, finishedFloatTime;
+  float floatSpeed, fadeInSpeed, fadeOutSpeed, finishedFadeInTime, finishedFloatTime;
 
   public FadeFloat(float floatDistance, float fadeInTime, float floatTime, float fadeOutTime) {
     super(fadeInTime + floatTime + fadeOutTime, 0, 0, 0);
@@ -10,9 +10,7 @@ public class FadeFloat extends Animation {
     this.fadeInSpeed = 1f / fadeInTime;
     this.fadeOutSpeed = 1f / fadeOutTime;
 
-    this.fadeInTime = fadeInTime;
-    this.floatTime = floatTime;
-    this.fadeOutTime = fadeOutTime;
+    this.finishedFadeInTime = fadeInTime;
     this.finishedFloatTime = fadeInTime + floatTime;
 
     this.floatSpeed = floatDistance / this.duration;
@@ -22,10 +20,12 @@ public class FadeFloat extends Animation {
     super.update(delta);
     y -= floatSpeed * delta;
 
-    if(progress >= finishedFloatTime) {
-      a -= fadeOutSpeed * delta;
-    } else if(progress <= fadeInTime) {
+    if(progress < finishedFadeInTime) {
       a += fadeInSpeed * delta;
+    } else if(progress >= finishedFloatTime) {
+      a -= fadeOutSpeed * delta;
+    } else {
+      a = 1f;
     }
 
   }
